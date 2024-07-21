1026c84e2978c2f65a48203023a223d22cc41460
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-07-21 08:17:32 +0530

    3rd-party-oauth

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,68 @@
-from flask import jsonify
+from flask import jsonify, session, redirect, url_for, request, abort, flash
+import secrets
+from urllib.parse import urlencode
+import requests
 
 from app.auth import bp
+from config import Config
 
-@bp.route("/register")
-def register():
-    return jsonify({})
+@bp.route('/authorize/<provider>')
+def oauth2_authorize(provider):
+
+    provider_data = Config.OAUTH2_PROVIDERS.get(provider)
+
+    session['oauth2_state'] = secrets.token_urlsafe(16)
+
+    qs = urlencode({
+        'client_id': provider_data['client_id'],
+        'redirect_uri': url_for('auth.oauth2_callback', provider=provider,
+                                _external=True),
+        'response_type': 'code',
+        'scope': ' '.join(provider_data['scopes']),
+        'state': session['oauth2_state'],
+    })
+
+    return redirect(provider_data['authorize_url'] + '?' + qs)
+
+
+@bp.route('/callback/<provider>')
+def oauth2_callback(provider):
+
+    provider_data = Config.OAUTH2_PROVIDERS.get(provider)
+
+    if 'error' in request.args:
+        for k, v in request.args.items():
+            if k.startswith('error'):
+                flash(f'{k}: {v}')
+        return redirect(url_for('index'))
+
+    if request.args['state'] != session.get('oauth2_state'):
+        abort(401)
+
+    if 'code' not in request.args:
+        abort(401)
+
+    response = requests.post(provider_data['token_url'], data={
+        'client_id': provider_data['client_id'],
+        'client_secret': provider_data['client_secret'],
+        'code': request.args['code'],
+        'grant_type': 'authorization_code',
+        'redirect_uri': url_for('auth.oauth2_callback', provider=provider,
+                                _external=True),
+    }, headers={'Accept': 'application/json'})
+    if response.status_code != 200:
+        abort(401)
+    oauth2_token = response.json().get('access_token')
+    if not oauth2_token:
+        abort(401)
+
+    response = requests.get(provider_data['userinfo']['url'], headers={
+        'Authorization': 'Bearer ' + oauth2_token,
+        'Accept': 'application/json',
+    })
+    if response.status_code != 200:
+        abort(401)
+        
+    email = provider_data['userinfo']['email'](response.json())
+
+    return email
--- config.py
+++ config.py
@@ -14,3 +14,39 @@ class Config:
     GITHUB_CLIENT_SECRET    = os.environ.get('GITHUB_CLIENT_SECRET')
 
     ImagekitID              = os.environ.get('ImagekitID')
+
+    OAUTH2_PROVIDERS        =  {
+        'google': {
+            'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
+            'client_secret': os.environ.get('GOOGLE_CLIENT_SECRET'),
+            'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+            'token_url': 'https://accounts.google.com/o/oauth2/token',
+            'userinfo': {
+                'url': 'https://www.googleapis.com/oauth2/v3/userinfo',
+                'email': lambda json: json['email'],
+            },
+            'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
+        },
+        'github': {
+            'client_id': os.environ.get('GITHUB_CLIENT_ID'),
+            'client_secret': os.environ.get('GITHUB_CLIENT_SECRET'),
+            'authorize_url': 'https://github.com/login/oauth/authorize',
+            'token_url': 'https://github.com/login/oauth/access_token',
+            'userinfo': {
+                'url': 'https://api.github.com/user/emails',
+                'email': lambda data: next((x['email'] for x in data if x['primary'] == True), None),
+            },
+            'scopes': ['user:email'],
+        },
+        'discord': {
+            'client_id': os.environ.get('DISCORD_CLIENT_ID'),
+            'client_secret': os.environ.get('DISCORD_CLIENT_SECRET'),
+            'authorize_url': 'https://discord.com/api/oauth2/authorize',
+            'token_url': 'https://discord.com/api/oauth2/token',
+            'userinfo': {
+                'url': 'https://discord.com/api/users/@me',
+                'email': lambda json: json['email'],
+            },
+            'scopes': ['identify email'],
+        },
+    }
