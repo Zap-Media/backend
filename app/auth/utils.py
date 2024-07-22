56749c93e9cfbc4d59064db0c2dbae98fde3969f
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-07-22 05:32:00 +0530

    sub user in tokens

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -6,10 +6,11 @@ from datetime import timedelta
 from config import Config
 from app import db
 
-def gen_tokens(user_obj, service):
+def gen_tokens(user_obj, sub_user, service):
     d = datetime.now()
     access_obj = {
         "_id": user_obj['_id'],
+        'sub_user': sub_user['_id'],
         "email": user_obj['email'],
         "service": service,
         "exp": d+timedelta(days=Config.TOKEN_EXP['access'])
@@ -35,34 +36,20 @@ def regen_tokens(refresh_token):
     except Exception as e:
         return {"success": False, "message": e}
     
-    d = datetime.now()
     _id = refresh_obj['_id']
     service = refresh_obj['service']
-    email = db.fetch_user("_id", _id)['email']
+    user = db.fetch_user("_id", _id)
 
-    new_access_obj = {
-        "_id": _id,
-        "email": email,
-        "service": service,
-        "exp": d+timedelta(days=Config.TOKEN_EXP['access'])
-    }
+    if service == 'zap-social':
+        sub_user = db.fetch_sub_user(db.social_users, "parent", _id)
 
-    new_refresh_obj = {
-        "_id": _id,
-        "service": service,
-        "exp": d+timedelta(days=Config.TOKEN_EXP['access'])
-    }
-    
-    return {
-        "success": True,
-        "access_token": jwt.encode(new_access_obj, Config.JWT_SECRET),
-        "refresh_token": jwt.encode(new_refresh_obj, Config.JWT_SECRET),
-    }
+    return gen_tokens(user, sub_user, service)
 
 def validate_access_token(access_token):
     try:
-        return jwt.decode(access_token, Config.JWT_SECRET)
+        decoded = jwt.decode(access_token, Config.JWT_SECRET)
     except jwt.ExpiredSignatureError:
         return {"success": False, "message": "access token expired"}
     except Exception as e:
         return {"success": False, "message": e}
+    return {"success": True, "obj": decoded}
