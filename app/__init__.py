fc33a9115da16476bf262ac921067e4aa3808089
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-07-27 05:14:44 +0530

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from config import Config
 from app.database import Database
@@ -7,6 +8,8 @@ db = Database()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
+    
     app.config.from_object(Config)
 
     from app.auth import bp as auth_bp
